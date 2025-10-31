9016a3a0554fa140d8d3cc841fc31c6d33129ef6
Michael <MichaelWidler@yahoo.com>
2025-10-31 07:22:36 -0700

    Get TimerPanel up and running.

--- ShadowdarkTracker/SDTFrame.java
+++ ShadowdarkTracker/SDTFrame.java
@@ -1,9 +1,18 @@
 package ShadowdarkTracker;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-public class SDTFrame extends JFrame
+public class SDTFrame extends JFrame implements ActionListener
 {
+   private TimerPanel timerPanel;
+   private SDTPanel[] characterPanel;
+   private SDTPanel controlPanel;
+   private javax.swing.Timer timer;
+   
+   public static final int CHARACTER_PANELS = 13;
+   
    public SDTFrame()
    {
       super();
@@ -11,6 +20,19 @@ public class SDTFrame extends JFrame
       setTitle("ShadowdarkTracker");
       setDefaultCloseOperation(EXIT_ON_CLOSE);
       
+      timer = new javax.swing.Timer(1000, null);
+      timer.start();
+      
+      setLayout(new GridLayout(CHARACTER_PANELS + 2, 1));
+      timerPanel = new TimerPanel(timer);
+      add(timerPanel);
+      
+      
       setVisible(true);
+   }
+   
+   public void actionPerformed(ActionEvent ae)
+   {
+   
    }
 }
---
+++ ShadowdarkTracker/TimerPanel.java
@@ -0,0 +1,99 @@
+package ShadowdarkTracker;
+
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+
+public class TimerPanel extends SDTPanel implements ActionListener
+{
+	private int curTime;
+	private int maxTime;
+	private boolean runF;
+	private JTextField timeF;
+	private JButton runB;
+	private JButton resetB;
+   private javax.swing.Timer timer;
+
+
+	public int getCurTime(){return curTime;}
+	public int getMaxTime(){return maxTime;}
+	public boolean isRunF(){return runF;}
+	public JTextField getTimeF(){return timeF;}
+	public JButton getRunB(){return runB;}
+	public JButton getResetB(){return resetB;}
+
+
+	public void setCurTime(int c){curTime = c;}
+	public void setMaxTime(int m){maxTime = m;}
+	public void setRunF(boolean r){runF = r;}
+	public void setTimeF(JTextField t){timeF = t;}
+	public void setRunB(JButton r){runB = r;}
+	public void setResetB(JButton r){resetB = r;}
+
+   public TimerPanel(javax.swing.Timer t)
+   {
+      maxTime = 60*60;
+      runF = false;
+      timeF = new JTextField("");
+      runB = new JButton("Start");
+      resetB = new JButton("Reset");
+      add(timeF);
+      add(runB);
+      runB.addActionListener(this);
+      add(resetB);
+      resetB.addActionListener(this);
+      arrangeElements();
+      timer = t;
+      timer.addActionListener(this);
+      resetTime();
+      displayTime();
+   }
+   
+   public void resetTime()
+   {
+      curTime = maxTime;
+   }
+   
+   public void actionPerformed(ActionEvent ae)
+   {
+      if(ae.getSource() == timer)
+      {
+         if(runF)
+         {
+            curTime = Math.max(curTime - 1, 0);
+         }
+      }
+      if(ae.getSource() == runB)
+      {
+         runF = !runF;
+         if(runF)
+            runB.setText("Pause");
+         else
+            runB.setText("Start");
+      }
+      if(ae.getSource() == resetB)
+      {
+         resetTime();
+      }
+      displayTime();
+   }
+   
+   public void displayTime()
+   {
+      timeF.setText(getTimeString());
+   }
+   
+   public void arrangeElements()
+   {
+      arrangeElement(timeF, .25, 0.0, .25, 1.0);
+      arrangeElement(runB, .5, 0.0, .25, 1.0);
+      arrangeElement(resetB, .75, 0.0, .25, 1.0);
+   }
+   
+   public String getTimeString()
+   {
+      int min = curTime / 60;
+      int sec = curTime % 60;
+      return String.format("%d:%02d", min, sec);
+   }
+}
